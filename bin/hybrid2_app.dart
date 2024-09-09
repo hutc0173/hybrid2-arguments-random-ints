3ef7bc16dc26a1d69941c5ddb6d3b4af09d267bf
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-09 17:50:04 -0400

    inital commit

---
+++ bin/hybrid2_app.dart
@@ -0,0 +1,5 @@
+import 'package:hybrid2_app/hybrid2_app.dart' as hybrid2_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hybrid2_app.calculate()}!');
+}
---
+++ test/hybrid2_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid2_app/hybrid2_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
