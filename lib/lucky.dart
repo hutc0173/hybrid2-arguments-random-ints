4d90c5b0566a4491a7cf0b3214f0b338bdb0d3b5
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-09-15 15:51:05 -0400

    first commit

--- lib/lucky.dart
+++ lib/lucky.dart
@@ -1,2 +1,21 @@
 import 'dart:io';
 import 'dart:math';
+
+class Lucky {
+  final int luckyNumber;
+
+  Lucky() : luckyNumber = _createRandomNumber();
+
+  static int _createRandomNumber() {
+    final minString = Platform.environment["MIN"];
+    final min = int.tryParse(minString ?? '0') ?? 0;
+
+    final maxString = Platform.environment["MAX"];
+    final max = int.tryParse(maxString ?? '100') ?? 100;
+
+    final randomNumber = Random()
+
+    // need to finish generating random number
+
+  }
+}
